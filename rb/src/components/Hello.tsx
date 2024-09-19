30fc22f06989f7b70f4db890812cc9e27aec5d4d
insun-k <kis8294@gmail.com>
2024-09-19 17:43:48 +0900

    component

---
+++ rb/src/components/Hello.tsx
@@ -0,0 +1,49 @@
+import { ReactNode, useState } from 'react';
+
+//const Title = (props: { text: string }) => <h1>{props.text}</h1>;
+
+// 가독성위해 type 별도로
+type TitleProps = {
+  text: string;
+  name: string;
+};
+
+//prop
+const Title = ({ text, name }: TitleProps) => (
+  <h1>
+    {text} {name}
+  </h1>
+); // destructuring 사용 가능
+
+// children
+const Body = ({ children }: { children: ReactNode }) => {
+  console.log('bbbbbb!!');
+  return <div className='red'>{children}</div>;
+};
+
+export default function Hello() {
+  // Hello => container component
+  const [myState, setMyState] = useState(0); // 상태
+  let v = 1;
+  console.log('**************', v, myState);
+  return (
+    <>
+      <Title text='Hi,' name='React' />
+      <Body>
+        This is Hello Body Component. {v} - {myState}
+      </Body>
+      <button
+        onClick={() => {
+          v++;
+          setMyState(myState + 1);
+          //console.log('v=', v);
+        }}
+      >
+        Click Here!
+      </button>
+    </>
+  );
+}
+
+// 버튼 누를때마다 component(Body), container component(Hello) 모두 실행됨 (console.log 참고)
+// => 불합리  => container component 적절한 사용 필요
