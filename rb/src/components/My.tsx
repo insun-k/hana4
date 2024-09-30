6a1480d39803b2f68a365e116f49f9395ff496c7
insun-k <kis8294@gmail.com>
2024-09-30 15:34:34 +0900

    tryThis 장바구니 상품 검색

--- rb/src/components/My.tsx
+++ rb/src/components/My.tsx
@@ -1,4 +1,4 @@
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaSearch } from 'react-icons/fa';
 // import { Session } from '../App.tsx';
 import Login, { LoginHandler } from './Login.tsx';
 import Profile from './Profile.tsx';
@@ -9,12 +9,13 @@ import {
   useLayoutEffect,
   useMemo,
   useRef,
+  useState,
 } from 'react';
 import Button from './atoms/Button.tsx';
 import { useSession } from '../hooks/session-context.tsx';
 import Item from './Item.tsx';
 import useToggle from '../hooks/toggle.ts';
-import { useTimeout } from '../hooks/timer-hooks.ts';
+import { useDebounce, useTimeout } from '../hooks/timer-hooks.ts';
 
 // type Props = {
 //   session: Session; // App에서 export한 type 사용
@@ -38,31 +39,18 @@ export default forwardRef(function My(
 
   const [isAdding, toggleAdding] = useToggle(true);
 
-  // const removeItem = (id: number) => {
-  //   // 삭제 확인 여부
-  //   if (confirm('Are u sure?')) {
-  //     removeCartItem(id);
-  //   }
-  // };
+  const [, toggleSearch] = useToggle();
+  const [searchstr, setSearchstr] = useState('');
+  const searchRef = useRef<HTMLInputElement>(null);
 
-  // const saveItem = (e: FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   const name = nameRef.current?.value;
-  //   const price = priceRef.current?.value;
-  //   if (!name) {
-  //     alert('상품명을 입력하세요');
-  //     return nameRef.current?.focus();
-  //   } else if (!price) {
-  //     alert('가격을 입력하세요');
-  //     return priceRef.current?.focus();
-  //   }
-  //   addCartItem(name, +price);
-
-  //   // save하고 input 초기화
-  //   nameRef.current.value = '';
-  //   priceRef.current.value = '';
-  //   nameRef.current.focus();
-  // };
+  useDebounce(
+    () => {
+      console.log('useDebounce.search>>', searchRef.current?.value);
+      setSearchstr(searchRef.current?.value || '');
+    },
+    1000,
+    [searchRef.current?.value]
+  );
 
   const totalPrice = useMemo(
     () => session.cart.reduce((acc, item) => acc + item.price, 0),
@@ -117,32 +105,47 @@ export default forwardRef(function My(
       ) : (
         <Login ref={ref} />
       )}
-
-      <ul className='my-3 w-2/3 border p-5'>
-        {session.cart?.length ? (
-          session.cart.map((item) => (
-            <li key={item.id}>
-              <Item item={item} />
-            </li>
-          ))
-        ) : (
-          <li className='text-gray-400'>no items</li>
-        )}
-
-        <li className='mt-3 text-center'>
-          {isAdding ? (
-            <Item
-              item={{ id: 0, name: '', price: 0 }}
-              toggleAdding={() => toggleAdding(true)}
-            />
+      <div className='m-4 w-2/3 border p-4'>
+        <div className='flex gap-3'>
+          <FaSearch />
+          <input
+            type='text'
+            placeholder='상품명 검색...'
+            // defaultValue={friend}
+            // onChange={(e) => setSearchstr(e.currentTarget.value)}
+            onChange={toggleSearch}
+            ref={searchRef}
+            className='inp'
+          />
+        </div>
+        <ul className='mt-3 px-3'>
+          {session.cart?.length ? (
+            session.cart
+              .filter(({ name }) => name.includes(searchstr))
+              .map((item) => (
+                <li key={item.id}>
+                  <Item item={item} />
+                </li>
+              ))
           ) : (
-            <Button onClick={toggleAdding}>
-              <FaPlus />
-              {'Add Item'}
-            </Button>
+            <li className='text-gray-400'>no items</li>
           )}
-        </li>
-      </ul>
+
+          <li className='mt-3 text-center'>
+            {isAdding ? (
+              <Item
+                item={{ id: 0, name: '', price: 0 }}
+                toggleAdding={() => toggleAdding(true)}
+              />
+            ) : (
+              <Button onClick={toggleAdding}>
+                <FaPlus />
+                {'Add Item'}
+              </Button>
+            )}
+          </li>
+        </ul>
+      </div>
       <div className='flex flex-col'>
         <span>총액 : {totalPrice.toLocaleString()}원</span>
         <span>할인 : {dcPrice.toFixed(0).toLocaleString()}원</span>
