0045b72e6fbb214ef4b2ce64e4457f951fb5b37f
insun-k <kis8294@gmail.com>
2024-10-07 10:45:29 +0900

    browser-router를 hashRouter로 변환

--- rb/src/main.tsx
+++ rb/src/main.tsx
@@ -3,15 +3,17 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { CounterProvider } from './hooks/counter-hook.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 // import Hello from './components/Hello.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CounterProvider>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
+      {/* </BrowserRouter> */}
     </CounterProvider>
   </StrictMode>
 );
