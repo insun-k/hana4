c211b429c735e9c1bd422a27f9c74c7541bd487f
insun-k <kis8294@naver.com>
2024-11-22 16:32:27 +0900

    view

--- demo/src/main/java/com/hana4/demo/controller/UserController.java
+++ demo/src/main/java/com/hana4/demo/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,12 @@ public class UserController {
 		this.service = service;
 	}
 
+	@GetMapping("/list")
+	public String userList(Model model) {
+		model.addAttribute("users", service.getList());
+		return "user/list";
+	}
+
 	@GetMapping("")
 	@ResponseBody
 	public List<User> getUsers() {
@@ -47,6 +54,12 @@ public class UserController {
 		} else {
 			throw new BadRequestException("Insert Error");
 		}
+	}
+
+	@PostMapping("/add")
+	public String addUser(@RequestBody User user) {
+		System.out.println("user = " + user);
+		return "redirect:/users/list";
 	}
 
 	@GetMapping("/{id}")
