c6367fd3fdc839fa3e60f72ccad074ef6219e292
insun-k <kis8294@naver.com>
2024-11-22 15:11:46 +0900

    user Controller

--- demo/src/main/java/com/hana4/demo/domain/User.java
+++ demo/src/main/java/com/hana4/demo/domain/User.java
@@ -4,6 +4,14 @@ public class User {
 	private Long id;
 	private String name;
 
+	public User() {
+		this("");
+	}
+
+	public User(String name) {
+		this(0L, name);
+	}
+
 	public User(Long id, String name) {
 		this.id = id;
 		this.name = name;
@@ -15,5 +23,13 @@ public class User {
 
 	public String getName() {
 		return name;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 }
